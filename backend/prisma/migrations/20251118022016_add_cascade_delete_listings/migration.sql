f33d4d3dbf8336acb679162a921a5bbfe1a2877c
ARCHI <archigarg9888@gmail.com>
2025-11-21 19:24:00 +0530

    backend update

--- backend/prisma/migrations/20251118022016_add_cascade_delete_listings/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `booking` DROP FOREIGN KEY `Booking_listingId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `Booking` ADD CONSTRAINT `Booking_listingId_fkey` FOREIGN KEY (`listingId`) REFERENCES `Listing`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
